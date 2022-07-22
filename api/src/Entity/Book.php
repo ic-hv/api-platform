2377846311c33453de5fe64629d4673ff2759771
Heiko Vogel <vogel@intercorp.de>
2022-07-22 11:06:20 +0200

    [TASK] Test-Example

---
+++ api/src/Entity/Book.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/** A book. */
+#[ApiResource]
+#[ORM\Entity]
+class Book {
+
+    /** The id of this book. */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    private ?int $id = null;
+
+    /** The ISBN of this book (or null if doesn't have one). */
+    #[ORM\Column(nullable: true)]
+    public ?string $isbn = null;
+
+    /** The title of this book. */
+    #[ORM\Column]
+    public string $title = '';
+
+    /** The description of this book. */
+    #[ORM\Column(type: 'text')]
+    public string $description = '';
+
+    /** The author of this book. */
+    #[ORM\Column]
+    public string $author = '';
+
+    /** The publication date of this book. */
+    #[ORM\Column]
+    public ?\DateTimeImmutable $publicationDate = null;
+
+    /** @var Review[] Available reviews for this book. */
+    #[ORM\OneToMany(mappedBy: 'book', targetEntity: Review::class, cascade: [ 'persist', 'remove' ])]
+    public iterable $reviews;
+
+    public function __construct() {
+        $this->reviews = [];//new ArrayCollection();
+    }
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+}
